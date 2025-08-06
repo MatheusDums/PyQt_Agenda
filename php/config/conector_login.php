0aebe953b8cae6642734271ab4e477fee566ce54
MatheusDums <dumss@icloud.com>
2025-08-06 14:45:14 -0300

    ainda no token

--- php/config/conector_login.php
+++ php/config/conector_login.php
@@ -2,6 +2,6 @@
 
 $pdo = new PDO('mysql:host=localhost;dbname=db_pyt_api', 'root', '');
 
-$stmt = $pdo->prepare('SELECT pyt_id, pyt_user, pyt_senha FROM tb_python_login');
+$stmt = $pdo->prepare('SELECT pyt_id, pyt_user, pyt_senha, pyt_token FROM tb_python_login');
 $stmt->execute();
 $dados_login = $stmt->fetchAll(PDO::FETCH_ASSOC);
