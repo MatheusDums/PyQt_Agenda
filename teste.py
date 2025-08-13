106db2329576b5337070819cc67e81c41af131d2
MatheusDums <dumss@icloud.com>
2025-08-13 14:45:45 -0300

    verificação de .ini

--- teste.py
+++ teste.py
@@ -1,15 +1,25 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from login_ok import Ui_Tela1
+import configparser
+import os
 
-class TelaLogin(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Tela1()
-        self.ui.setupUi(self)
+config = configparser.ConfigParser()
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    login = TelaLogin()
-    login.showMaximized()
-    sys.exit(app.exec())
+if os.path.exists("application.ini"):
+    class TelaLogin(QMainWindow):
+        def __init__(self):
+            super().__init__()
+            self.ui = Ui_Tela1()
+            self.ui.setupUi(self)
+
+    if __name__ == "__main__":
+        app = QApplication(sys.argv)
+        login = TelaLogin()
+        login.showMaximized()
+        sys.exit(app.exec())
+       
+else:
+       print("Erro: Arquivo config.ini não encontrado.")
+       exit()
+
